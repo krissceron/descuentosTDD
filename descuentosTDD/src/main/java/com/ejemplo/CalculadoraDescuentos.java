f0d15fbaa1d566b7444c6bcda21e76af9b637038
Christian Jácome <129555977+ChrisJMora@users.noreply.github.com>
2025-06-02 01:29:15 +0000

    Introduciendo errores

--- descuentosTDD/src/main/java/com/ejemplo/CalculadoraDescuentos.java
+++ descuentosTDD/src/main/java/com/ejemplo/CalculadoraDescuentos.java
@@ -1,12 +1,18 @@
 package com.ejemplo;
 
 public class CalculadoraDescuentos {
-    public static double aplicarDescuentoPorcentaje(double monto, double porcentaje) {
-        if (porcentaje > 100) return 0;
-        return monto - (monto * porcentaje/100); // hardcoded para pasar el test
+    public static double aplicarDescuentoPorcentaje(double monto,double porcentaje) {
+        if (porcentaje>100) return 0;
+        return monto-(monto*porcentaje/100); // hardcoded para pasar el test
     }
 
-    public static double aplicarDescuentoFijo(double monto, double descuento){
-        return monto - descuento;
+    public static double aplicarDescuentoFijo(double monto,double descuento) {
+        return monto-descuento;
+    }
+
+    static public void otroMetodo() {
+        double a = 1;
+        a = 1;
+        a = 1;
     }
 }
